5769ae06f6dc98a68fc8f39120ae3ecf33dd5e76
Bruno Máximo <Bmaximo93@gmail.com>
2025-03-29 20:37:51 -0300

    :hammer_and_wrench: Reunião inicial de alinhamento, criação de funções básicas e estruturas

--- Projeto_ED1.cpp
+++ Projeto_ED1.cpp
@@ -8,11 +8,25 @@
 #include <unistd.h> // Caso o sistema seja MacOS/Linux
 #endif
 
+#define MAX 60
 
+void cpClear() { 
+	#ifdef _WIN32
+        system("cls");  
+    #else
+        system("clear"); 
+    #endif
+}
 
-typedef struct disciplina{
+typedef struct disciplina { 
+	/*
+	coloquei disciplina como um struct pra talvez a gnt tentar criar um array de disciplinas prévio, mas isso não é necessário
+	os dados podem ficar no node, o que acham?
+	*/
 	int id;
 	char nome[50];
+	int nota;
+
 } _disciplina; 
 
 typedef struct node {
@@ -22,24 +36,34 @@ typedef struct node {
 
 } _node;
 
-typedef _node* _lista;
+typedef _node* L_lista;
 
 typedef struct aluno {
 	
 	int RGM;
 	char nome[50];
-	_lista *disciplinas; 
+	L_lista *disciplinas; 
 
 } _aluno;
 
-_aluno alunos[60];
+typedef struct S_lista {
+
+	_aluno alunos[MAX];
+	int n;
+
+} S_lista;
+
+S_lista criar();
+void continuar();
+ //_disciplina disciplinas[40] = {43202, "Estruturas de Dados", 0}{3222, "Banco de dados", 0}{3, "POO", 0};
+
 
 void mostrarMenu() {
-	printf("1. Criar lista\n");
-	printf("2. Cadastrar disciplina\n");
-	printf("3. Mostrar disciplinas\n");
-	printf("4. Cadastrar aluno\n");
-	printf("5. Mostrar alunos\n");
+	printf("1. Criar lista\n"); //remover criar lista
+	printf("2. Inserir novo aluno\n"); //->informa qual aluno->inserir disciplina na lista de dsicplinas do aluno
+	printf("3. Mostrar disciplinas\n"); 
+	printf("4. Mostrar alunos\n");
+	printf("5. Matricular aluno em disciplinas\n");//cadastra aluno, pede pra inserir disciplinas ate o usuario terminar
 	printf("6. Remover aluno\n");
 	printf("7. Remover disciplina\n");
 	printf("8. Créditos\n");
@@ -47,17 +71,12 @@ void mostrarMenu() {
 }
 
 // cpClear significa aqui "Cross Platform Clear", para que o programa funcione bem no Windows, MacOS e Linux.
-void cpClear() { 
-	#ifdef _WIN32
-        system("cls");  
-    #else
-        system("clear"); 
-    #endif
-}
+
 
 int main() {
 	setlocale(LC_ALL, 0);
 	int opcao;
+	S_lista alunos;
 
 	do {
 		mostrarMenu();
@@ -70,11 +89,13 @@ int main() {
 			case 1:
 				static int criado = 0; // Para inicializar vari�veis dentro do switch-case, é necessário ou criar um escopo ou usar variável estática. Preferi usar static.
 				if (!criado) { // Para não deixar criar a lista duas vezes
-						// lógica
+						alunos = criar();
+						criado = 1;// lógica
 					}
 					else {
 						printf("Erro: lista já foi criada\n");
 					}
+				continuar();
 				break;
 			case 2:
 				if (criado) { // Todos os cases que envolvem manipular a lista tem essa lógica, para que só possa manipular a lista se ela já tiver sido criada.
@@ -140,6 +161,17 @@ int main() {
 	return 0;
 }
 
+S_lista criar() {
+
+    S_lista lista;
+
+    lista.n = -1;
+
+	printf("\nLista foi criada\n");
+    return lista;
+
+}
+
 _node * criarNode() {
 
 _node *n = (_node*) malloc(sizeof(_node));
@@ -154,8 +186,9 @@ return NULL;
 
 }
 
+//função buscar disciplina
 
-int inserirDisciplina(_lista *disciplinas, _disciplina nova_disciplina) {
+int inserirDisciplina(L_lista *disciplinas, _disciplina nova_disciplina) {
 	
 	_node *novo_node = criarNode();
 	if (novo_node == NULL)
@@ -182,14 +215,14 @@ int inserirDisciplina(_lista *disciplinas, _disciplina nova_disciplina) {
 	return 0;
 
 }
-
+/*
 _aluno novo_aluno()
 {
 
-	//novo aluno, tem que alocar memoria para a lista de disciplinas, e inicializar ela como vazia
-	//tb inserir o aluno de forma ordenada na lista, em ordem de RGM se a lista(de alunos) não estiver vazia
+	novo aluno, tem que alocar memoria para a lista de disciplinas, e inicializar ela como vazia
+	tb inserir o aluno de forma ordenada na lista, em ordem de RGM se a lista(de alunos) não estiver vazia
 
-	/*
+	
 		pensei em duas formas de implementar isso, 1. podemos inserir toda a logica relacionada a criar um novo aluno aqui
 		por exemplo pedir o input do RGM e Nome e em seguida usar o RGM pra posicionar o aluno ordenadamente na lista
 		(a busca binária pra inserir o RGM no indice correto da lista seria uma função separada)
@@ -200,5 +233,12 @@ _aluno novo_aluno()
 		mas acho q faz mais sentido colocar td relacionado a inserir o aluno nessa função, com uma função separada pra descobrir o indice
 		pra inserir ordenadamente o aluno na lista
 
-	*/
+	
+}
+
+*/
+
+void continuar(){
+	printf("\nAperte 'ENTER' para continuar\n");
+	getchar();
 }
